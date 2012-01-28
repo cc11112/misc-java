30dbf3df410e6deecc8d7a41a637ba694820f16b
Crane <szcshui@gmail.com>
2012-01-28 16:54:18 -0600

    fixed output format

--- src/misc/BigNumber.java
+++ src/misc/BigNumber.java
@@ -9,44 +9,34 @@ public class BigNumber {
 		// get how many number of points
 		final int size = 10;
 
+		System.out.println("productsize \t duration(ms)");
+		System.out.println("----------------------------");
+		
 		new BigNumber().Run(size);
 
+		System.out.println("----------------------------");
 		System.out.println("Done");
 	}
 
 	public void Run(int size) {
-
+		
 		Random r = new Random();
 
 		long bigIntegerSize = 5000; // initial size
-
-		long[][] data = new long[size][2];
 
 		for (int i = 0; i < size; ++i) {
 
 			BigInteger bigInteger1 = GenerateBigInteger(bigIntegerSize, r);
 			BigInteger bigInteger2 = GenerateBigInteger(bigIntegerSize + 1, r);
 
-			data[i][0] = bigIntegerSize;
-			data[i][1] = calculate(bigInteger1, bigInteger2);
+			long ms = calculate(bigInteger1, bigInteger2);
 
+			System.out.println(bigIntegerSize + "\t" + ms);
+			
 			// get next big Integer, 5000 is step
 			bigIntegerSize += 5000;
-			
-			System.out.println("product size:" + data[i][0] + " duration: " + data[i][1]);
 		}
-//
-//		System.out.println("---for csv format");
-//		
-//		for (int i = 0; i < size; ++i) {
-//			System.out.println(data[i][0]);
-//		}
-//
-//		System.out.println("-------------------");
-//
-//		for (int i = 0; i < size; ++i) {
-//			System.out.println(data[i][1]);
-//		}
+		
 	}
 
 	private BigInteger GenerateBigInteger(long digits, Random r) {
@@ -55,12 +45,17 @@ public class BigNumber {
 
 		for (long i = 0; i < digits; ++i) {
 			char c = Double.toString(r.nextDouble() * 100).charAt(0);
+			
 			// only handle first digit
-			// if the first digit is zero, try again.
-			if (i == 0 && c == '0') {
+			if (i == 0 && c == '0') 
+			{
+				// if the first digit is zero, try again.
 				i = -1;
 			}
-			sb.append(c);
+			else
+			{
+				sb.append(c);
+			}
 		}
 
 		return new BigInteger(sb.toString());
@@ -68,10 +63,10 @@ public class BigNumber {
 
 	private long calculate(BigInteger bigInteger1, BigInteger bigInteger2) {
 
-		System.out.println("bigInteger1 Length:"
-				+ bigInteger1.toString().length());
-		System.out.println("bigInteger2 Length:"
-				+ bigInteger2.toString().length());
+//		System.out.println("bigInteger1 Length:"
+//				+ bigInteger1.toString().length());
+//		System.out.println("bigInteger2 Length:"
+//				+ bigInteger2.toString().length());
 
 		long startTime = System.currentTimeMillis();
 
